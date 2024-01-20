fc0ca4a59e7fef49e9442525f8cd9b9e041acd73
Maxxiandrade <maxiandrade62@gmail.com>
2024-01-20 01:47:33 -0300

    admin features on detail

--- athletiX/src/components/Home/Detail/Detail.jsx
+++ athletiX/src/components/Home/Detail/Detail.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Navbar/Navbar'
-import items from "../../Cards/items"
-import { redirect, useParams, useNavigate } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import carrito from "../../../assets/carrito.svg"
-import { Link } from 'react-router-dom'
+
 import { addToCart } from '../../../redux/actions/actions'
 import { useDispatch, useSelector } from 'react-redux'
 import Swal from 'sweetalert2'
 import shopcart from "../../../assets/shopcart.svg"
+import { TbShoppingCartX, TbEraserOff } from "react-icons/tb";
+import pen from "../../../assets/pen.svg"
+import axios from 'axios'
+import done from "../../../assets/done.svg"
 
 const Detail = () => {
   const navigate = useNavigate()
@@ -15,7 +18,10 @@ const Detail = () => {
     const {id} = useParams()
     const items = useSelector((state)=>state.allItems)
     const item = items.filter((item)=>item.id == id)
-    console.log(item);
+    const user = useSelector((state) => state.userInfo);
+    const isAdmin = user.isAdmin;
+    const [price, setPrice] = useState(item[0]?.precio)
+    const [togglePrice, setTogglePrice] = useState(false)
 
     const handleAddToCart = (id)=>{
       Swal.fire({
@@ -37,6 +43,59 @@ const Detail = () => {
       
     }
 
+    const handlePrice = (e) => {
+      const newPrice = e.target.value;
+      setPrice((prevPrice) => {
+        // Use the previous price to update state
+        if (prevPrice !== newPrice) {
+          updatePrice(id, newPrice);
+        }
+        return newPrice;
+      });
+    };
+    
+    const updatePrice = async (id, newPrice) => {
+      try {
+        await axios.put('http://localhost:3001/price', {
+          id,
+          price: newPrice
+        });
+      } catch (error) {
+        throw Error(error);
+      }
+    };
+    
+    const handleDone = () => {
+      Swal.fire({
+        title: 'Price updated successfully!',
+        showConfirmButton: true,
+        confirmButtonText: 'Ok!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate('/admin');
+        }
+      });
+      setTogglePrice(false);
+    };
+    
+    const outOfStock = () => {
+      setPrice((prevPrice) => {
+        if (prevPrice !== 0) {
+          updatePrice(id, 0);
+        }
+        return 0;
+      });
+    
+      Swal.fire({
+        title: 'Removed from stock successfully!',
+        showConfirmButton: true,
+        confirmButtonText: 'Ok!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate('/admin');
+        }
+      });
+    };
   return (
     <>
   <Navbar/>
@@ -52,10 +111,18 @@ const Detail = () => {
             <p>{item.descripcion}</p>
             <br />
             <div className="flex items-center">
-              <p className="ml-3">Price: ${item.precio}</p>
+              {<p className="ml-3">Price: $ {togglePrice ? <input type='text' className='w-24' onChange={handlePrice} value={price}/> : item.precio}</p>}
               <button className='hover:scale-110'>
                 <img src={carrito} alt="" className='size-10' onClick={() => handleAddToCart(item.id)} />
               </button>
+              {isAdmin && 
+              <>
+              {togglePrice ? <img src={done} name='confirm' className='h-8 cursor-pointer hover:scale-110' onClick={handleDone}/> :
+               <img src={pen} alt="" className='h-8 cursor-pointer hover:scale-110' onClick={()=>setTogglePrice(!togglePrice)}/>}
+              <TbShoppingCartX className='cursor-pointer hover:scale-110' onClick={()=>outOfStock()}/>
+              <TbEraserOff className='cursor-pointer hover:scale-110'/>
+              </>}
+              
             </div>
           </div>
         </div>
